ae4989b02c2dc9bf52881a7b5b271a0a024ef2b5
sakurakyuo <sakurakyuo@protonmail.com>
2023-12-21 00:04:58 +0800

    udon: truck sm8475 and kernel modules

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,7 +9,8 @@ rm -rf hardware/oplus
 git clone https://github.com/SakuraKyuoDevelopment/android_device_oneplus_sm8450-common device/oneplus/sm8450-common
 git clone --depth=1 https://gitlab.com/AdarshGrewal/proprietary_vendor_oneplus_udon vendor/oneplus/udon
 git clone --depth=1 https://gitlab.com/AdarshGrewal/proprietary_vendor_oneplus_sm8450-common vendor/oneplus/sm8450-common
-git clone --depth=1 https://github.com/SakuraKyuoDevelopment/android_kernel_oneplus_sm8450 kernel/oneplus/sm8450
+git clone --depth=1 https://github.com/lineage-ovaltine-dev/android_kernel_oneplus_sm8475 kernel/oneplus/sm8450
+git clone https://github.com/lineage-ovaltine-dev/android_kernel_oneplus_sm8450-modules kernel/oneplus/sm8450-modules
 git clone https://github.com/LineageOS/android_hardware_oneplus -b lineage-21 hardware/oneplus
 git clone https://github.com/LineageOS/android_hardware_oplus -b lineage-21 hardware/oplus
 # Oneplus moment
